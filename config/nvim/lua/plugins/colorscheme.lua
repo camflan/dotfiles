a765e245d68dd6b31862f46e911e747d6d9f88dd
Camron Flanders <camron.flanders@gmail.com>
2023-05-22 16:19:44 -0500

    switch to gh theme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,22 @@
 local M = {
-
-	-- tokyonight
+	-- github
 	{
-		"folke/tokyonight.nvim",
-		lazy = true,
-		opts = { style = "moon" },
+		"projekt0n/github-nvim-theme",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("github-theme").setup({
+				-- ...
+			})
+
+			vim.cmd("colorscheme github_dark")
+		end,
 	},
 
 	-- catppuccin
 	{
 		"catppuccin/nvim",
-		lazy = false,
+		lazy = true,
 		name = "catppuccin",
 		priority = 1000, -- load as soon as possible
 		dependencies = {
@@ -18,7 +24,6 @@ local M = {
 		},
 		config = function()
 			local catppuccin = require("catppuccin")
-
 			vim.g.catppuccin_flavour = "macchiato"
 
 			catppuccin.setup({
@@ -33,29 +38,38 @@ local M = {
 				},
 			})
 
-			vim.cmd([[colorscheme catppuccin]])
+			-- vim.cmd([[colorscheme catppuccin]])
 		end,
 	},
+
+	-- tokyonight
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+		opts = { style = "moon" },
+	},
+
 	{
 		"altercation/vim-colors-solarized",
 		lazy = true,
 	},
+
 	{
 		"srcery-colors/srcery-vim",
 		lazy = true,
 	},
-	{ "tjammer/blayu.vim", lazy = true },
-	{ "tomasr/molokai", lazy = true },
-	{ "mhartington/oceanic-next", lazy = true },
+	{ "tjammer/blayu.vim",                lazy = true },
+	{ "tomasr/molokai",                   lazy = true },
+	{ "mhartington/oceanic-next",         lazy = true },
 	{ "marciomazza/vim-brogrammer-theme", lazy = true },
-	{ "haishanh/night-owl.vim", lazy = true },
-	{ "sts10/vim-pink-moon", lazy = true },
-	{ "fenetikm/falcon", lazy = true },
-	{ "phanviet/vim-monokai-pro", lazy = true },
-	{ "andreypopp/vim-colors-plain", lazy = true },
-	{ "cocopon/iceberg.vim", lazy = true },
-	{ "dracula/vim", name = "dracula", lazy = true },
-	{ "pineapplegiant/spaceduck", lazy = true },
+	{ "haishanh/night-owl.vim",           lazy = true },
+	{ "sts10/vim-pink-moon",              lazy = true },
+	{ "fenetikm/falcon",                  lazy = true },
+	{ "phanviet/vim-monokai-pro",         lazy = true },
+	{ "andreypopp/vim-colors-plain",      lazy = true },
+	{ "cocopon/iceberg.vim",              lazy = true },
+	{ "dracula/vim",                      name = "dracula", lazy = true },
+	{ "pineapplegiant/spaceduck",         lazy = true },
 }
 
 return M

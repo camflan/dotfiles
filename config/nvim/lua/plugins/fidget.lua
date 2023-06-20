1d1f4e6c61d35b13aea14258b8c7e3f1425c66b3
Camron Flanders <camron.flanders@gmail.com>
2023-06-20 10:15:11 -0500

    pin fidget for now

--- config/nvim/lua/plugins/fidget.lua
+++ config/nvim/lua/plugins/fidget.lua
@@ -1,5 +1,8 @@
+-- https://github.com/j-hui/fidget.nvim
+-- Used to show LSP Progress in lower right
 return {
-	"j-hui/fidget.nvim",
-	lazy = false,
-	config = true,
+  "j-hui/fidget.nvim",
+  tag = "legacy", -- used temporarily while fidget is rewritten
+  lazy = false,
+  config = true,
 }

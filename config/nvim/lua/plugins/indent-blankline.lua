9ce4d85be2681de4654fee274b549f635ef885ea
Camron Flanders <camron.flanders@gmail.com>
2023-09-12 08:42:16 -0500

    do not show start

--- config/nvim/lua/plugins/indent-blankline.lua
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -8,10 +8,10 @@ return {
 
     opts = {
       show_current_context = true,
-      show_current_context_start = true,
+      show_current_context_start = false,
       show_first_indent_level = true,
       -- use_treesitter = true,
       -- use_treesitter_scope  = true,
-    }
+    },
   },
 }

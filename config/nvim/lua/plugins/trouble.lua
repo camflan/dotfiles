e45e0899758bdcfe4441f713729f48427f9e32b4
Camron Flanders <camron.flanders@gmail.com>
2023-11-16 00:14:24 -0600

    formatting

--- config/nvim/lua/plugins/trouble.lua
+++ config/nvim/lua/plugins/trouble.lua
@@ -1,33 +1,33 @@
 return {
-	{
-		"folke/trouble.nvim",
-		keys = {
-			{
-				"<leader>T",
-				":TroubleToggle<CR>",
-				mode={ "n" },
-				remap = false,
-				silent = true
-			}
-		},
-		config = function()
-			local trouble = require("trouble")
+  {
+    "folke/trouble.nvim",
+    keys = {
+      {
+        "<leader>T",
+        ":TroubleToggle<CR>",
+        mode = { "n" },
+        remap = false,
+        silent = true,
+      },
+    },
+    config = function()
+      local trouble = require("trouble")
 
-			trouble.setup({
-					position = "bottom",
-					icons = false,
-					fold_open = "v", -- icon used for open folds
-					fold_closed = ">", -- icon used for closed folds
-					indent_lines = false, -- add an indent guide below the fold icons
-					signs = {
-						-- icons / text used for a diagnostic
-						error = "error",
-						warning = "warn",
-						hint = "hint",
-						information = "info",
-					},
-					use_diagnostic_signs = false,
-				})
-		end,
-	},
+      trouble.setup({
+        position = "bottom",
+        icons = false,
+        fold_open = "v", -- icon used for open folds
+        fold_closed = ">", -- icon used for closed folds
+        indent_lines = false, -- add an indent guide below the fold icons
+        signs = {
+          -- icons / text used for a diagnostic
+          error = "error",
+          warning = "warn",
+          hint = "hint",
+          information = "info",
+        },
+        use_diagnostic_signs = false,
+      })
+    end,
+  },
 }

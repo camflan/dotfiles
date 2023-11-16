e3dd90e256bfa72a9d6597f88f116950d2bfde6e
Camron Flanders <camron.flanders@gmail.com>
2023-11-15 22:34:23 -0600

    add asdf plugins, update tailwindcss lsp config

--- config/nvim/lua/plugins/git-conflict.lua
+++ config/nvim/lua/plugins/git-conflict.lua
@@ -5,14 +5,6 @@ return {
       local git_conflict = require("git-conflict")
 
       git_conflict.setup({
-          default_mappings = {
-            ours = 'o',
-            theirs = 't',
-            none = '0',
-            both = 'b',
-            next = 'n',
-            prev = 'p',
-          },
         disable_diagnostics = true,
         highlights = {
           incoming = "DiffText",

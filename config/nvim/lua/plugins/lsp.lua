bbb385dc9988c0a727070ab7e552ae88ea390208
Camron Flanders <camron.flanders@gmail.com>
2023-11-28 13:10:08 -0600

    increase timeout for formatters

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -275,7 +275,7 @@ return {
     opts = {
       formatters_by_ft = formatters_by_ft,
       -- Set up format-on-save
-      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      format_on_save = { timeout_ms = 1000, lsp_fallback = true },
     },
     -- config = function(_, opts)
     --   require("conform").setup(opts)

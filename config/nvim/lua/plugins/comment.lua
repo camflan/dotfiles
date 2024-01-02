af429e52731f1b77c529e1b3581e6ffa8876e88a
Camron Flanders <me@camronflanders.com>
2024-01-02 11:33:18 -0600

    add krew, update gitignore

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -54,4 +54,13 @@ return {
       })
     end,
   },
+  {
+    "PatschD/zippy.nvim",
+    lazy = true,
+    event = { "LspAttach" },
+    keys = {
+      { "<leader>dl", ':lua require("zippy").insert_print()<CR>', desc = "Insert debug log" },
+    },
+    opts = {},
+  },
 }

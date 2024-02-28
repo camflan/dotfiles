797c9275ab27ad18d3e51f3813968f68fcf7756a
Camron Flanders <me@camronflanders.com>
2024-02-28 16:31:24 -0600

    try gen.nvim for ai

--- config/nvim/lua/plugins/ai-llm.lua
+++ config/nvim/lua/plugins/ai-llm.lua
@@ -1,6 +1,37 @@
 return {
   {
+    "David-Kunz/gen.nvim",
+    opts = {
+      model = "zephyr", -- The default model to use.
+      -- host = "localhost", -- The host running the Ollama service.
+      -- port = "11434", -- The port on which the Ollama service is listening.
+      display_mode = "split", -- The display mode. Can be "float" or "split".
+      show_prompt = false, -- Shows the Prompt submitted to Ollama.
+      show_model = true, -- Displays which model you are using at the beginning of your chat session.
+      no_auto_close = false, -- Never closes the window automatically.
+      init = function()
+        pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
+      end,
+      -- Function to initialize Ollama
+      -- command = function(options)
+      --   return "curl --silent --no-buffer -X POST http://"
+      --     .. options.host
+      --     .. ":"
+      --     .. options.port
+      --     .. "/api/generate -d $body"
+      -- end,
+      -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+      -- This can also be a command string.
+      -- The executed command must return a JSON object with { response, context }
+      -- (context property is optional).
+      -- list_models = '<omitted lua function>', -- Retrieves a list of model names
+      debug = false, -- Prints errors and the command which is run.
+    },
+  },
+
+  {
     "dustinblackman/oatmeal.nvim",
+    enabled = false,
     cmd = { "Oatmeal" },
     keys = {
       { "<leader>om", mode = "n", desc = "Start Oatmeal session" },
@@ -12,6 +43,7 @@ return {
   },
   {
     "nomnivore/ollama.nvim",
+    enabled = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
     },

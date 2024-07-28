af93e8c5d1378b6e6375bea6b472769866368f74
Camron Flanders <me@camronflanders.com>
2024-07-28 13:31:18 -0500

    add todo mapping, add ui configs

---
+++ config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,25 @@
+return {
+  -- https://github.com/stevearc/dressing.nvim
+  {
+    "stevearc/dressing.nvim",
+    enabled = true,
+    opts = {},
+  },
+
+  -- https://github.com/folke/noice.nvim
+  {
+    "folke/noice.nvim",
+    enabled = false,
+    event = "VeryLazy",
+    opts = {
+      cmdline = {
+        view = "cmdline",
+      },
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+}

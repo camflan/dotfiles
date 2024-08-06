4aaa1d3361b2e24076337638940af98da3fa6417
Camron Flanders <me@camronflanders.com>
2024-08-06 12:03:12 -0500

    try lualine

--- config/nvim/lua/plugins/statusline/init.lua
+++ config/nvim/lua/plugins/statusline/init.lua
@@ -2,5 +2,80 @@
 -- return require("plugins.statusline.heirline")
 
 -- My windline config
-return require("plugins.statusline.my-evil-windline")
+-- return require("plugins.statusline.my-evil-windline")
 -- return require("plugins.statusline.windline")
+
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = { "VeryLazy" },
+    opts = {
+      extensions = {
+        "fugitive",
+        "fzf",
+        "lazy",
+        "mason",
+        "oil",
+        "trouble",
+      },
+      options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        },
+      },
+      sections = {
+        lualine_a = {
+          {
+            "mode",
+            fmt = function(str)
+              return str:sub(1, 1)
+            end,
+          },
+        },
+        lualine_b = { "branch", "diff" },
+        lualine_c = {
+          {
+            "filename",
+            file_status = true,
+            newfile_status = true,
+            path = 4,
+            symbols = {
+              newfile = "[N]",
+            },
+          },
+        },
+        lualine_x = { "diagnostics" },
+        lualine_y = {},
+        lualine_z = {
+          { "location", separator = "", padding = { left = 1, right = 0 } },
+          { "progress", separator = "", padding = { left = 0, right = 1 } },
+        },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+    },
+  },
+}

18995fdc5f11562a6ac0dbc631d0f8dec5b6e154
Camron Flanders <me@camronflanders.com>
2024-12-10 10:02:38 -0600

    fix harper condition

--- config/nvim/lua/plugins/lsp/init.lua
+++ config/nvim/lua/plugins/lsp/init.lua
@@ -472,33 +472,35 @@ return {
         },
       })
 
-      lsp_config.harper_ls.setup({
-        capabilities = capabilities,
-        on_attach = common.on_attach,
-        flags = common.flags,
-        settings = {
-          ["harper-ls"] = {
-            linters = {
-              spell_check = true,
-              spelled_numbers = false,
-              an_a = true,
-              sentence_capitalization = false,
-              unclosed_quotes = true,
-              wrong_quotes = false,
-              long_sentences = true,
-              repeated_words = true,
-              spaces = true,
-              matcher = true,
-              correct_number_suffix = true,
-              number_suffix_capitalization = true,
-              multiple_sequential_pronouns = true,
-              linking_verbs = false,
-              avoid_curses = true,
-              terminating_conjunctions = true,
+      if constants.flags.USE_HARPER then
+        lsp_config.harper_ls.setup({
+          capabilities = capabilities,
+          on_attach = common.on_attach,
+          flags = common.flags,
+          settings = {
+            ["harper-ls"] = {
+              linters = {
+                spell_check = true,
+                spelled_numbers = false,
+                an_a = true,
+                sentence_capitalization = false,
+                unclosed_quotes = true,
+                wrong_quotes = false,
+                long_sentences = true,
+                repeated_words = true,
+                spaces = true,
+                matcher = true,
+                correct_number_suffix = true,
+                number_suffix_capitalization = true,
+                multiple_sequential_pronouns = true,
+                linking_verbs = false,
+                avoid_curses = true,
+                terminating_conjunctions = true,
+              },
             },
           },
-        },
-      })
+        })
+      end
 
       local schemastore = require("schemastore")
 

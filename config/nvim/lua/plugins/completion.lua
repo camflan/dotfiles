421b7ec151deba8b153706d31e81b36b4fa9a032
Camron Flanders <me@camronflanders.com>
2024-12-16 12:21:25 -0600

    auto show docs

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -45,6 +45,11 @@ return {
         enabled = true,
       },
 
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 500,
+      },
+
       appearance = {
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
         -- Useful for when your theme doesn't support blink.cmp

866891e3817e41135ea190962a392ff8ff5c8f18
Camron Flanders <me@camronflanders.com>
2025-01-23 11:42:09 -0600

    add folke-snacks

---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  -- https://github.com/folke/snacks.nvim
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      bigfile = { enabled = true },
+    },
+  },
+}

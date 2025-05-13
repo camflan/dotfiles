b7b5f879f68ab40880ac9bbc0ff1140b8c1024d2
Camron Flanders <me@camronflanders.com>
2025-05-13 09:58:51 -0500

    format

--- config/nvim/lsp/tailwindcss.lua
+++ config/nvim/lsp/tailwindcss.lua
@@ -1,25 +1,25 @@
 vim.lsp.config("tailwindcss", {
-    settings = {
-        tailwindCSS = {
-            classAttributes = {
-                "class",
-                "className",
-                "ngClass",
-                ".*Class",
-                ".*Classes",
-                ".*ClassNames",
-                ".*CLASSNAMES",
-                ".*Styles",
-            },
-            experimental = {
-                classRegex = {
-                    { "cva\\(((?:[^()]|\\([^()]*\\))*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
-                    { "classnames\\(([^)]*)\\)", "'([^']*)'" },
-                    { "cx\\(([^)]*)\\)", "'([^']*)'" },
-                    { "clsx\\(([^)]*)\\)", "'([^']*)'" },
-                    -- { "cva\\(([^)]*)\\)", "'([^']*)'" },
-                },
-            },
+  settings = {
+    tailwindCSS = {
+      classAttributes = {
+        "class",
+        "className",
+        "ngClass",
+        ".*Class",
+        ".*Classes",
+        ".*ClassNames",
+        ".*CLASSNAMES",
+        ".*Styles",
+      },
+      experimental = {
+        classRegex = {
+          { "cva\\(((?:[^()]|\\([^()]*\\))*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+          { "classnames\\(([^)]*)\\)", "'([^']*)'" },
+          { "cx\\(([^)]*)\\)", "'([^']*)'" },
+          { "clsx\\(([^)]*)\\)", "'([^']*)'" },
+          -- { "cva\\(([^)]*)\\)", "'([^']*)'" },
         },
+      },
     },
+  },
 })
--- config/nvim/lsp/vtsls.lua
+++ config/nvim/lsp/vtsls.lua
@@ -7,12 +7,12 @@ local group = vim.api.nvim_create_augroup(AUGROUP, { clear = true })
 
 --- @return table
 local function get_buffer_map(bufnr)
-    local ok, maps = pcall(vim.api.nvim_buf_get_var, bufnr, MAPGROUP)
-    if ok then
-        return maps or {}
-    else
-        return {}
-    end
+  local ok, maps = pcall(vim.api.nvim_buf_get_var, bufnr, MAPGROUP)
+  if ok then
+    return maps or {}
+  else
+    return {}
+  end
 end
 
 --- Remember the mapping in a buffer variable, so the bindings can be removed
@@ -25,59 +25,52 @@ end
 --- @param rhs string|function
 --- @param opts table
 local function remember_buffer_map(buf, modes, lhs, rhs, opts)
-    local _opts = opts or {}
-    _opts["buffer"] = buf
+  local _opts = opts or {}
+  _opts["buffer"] = buf
 
-    vim.keymap.set(modes, lhs, rhs, _opts)
+  vim.keymap.set(modes, lhs, rhs, _opts)
 
-
-    local maps = get_buffer_map(buf)
-    table.insert(maps or {}, { modes = modes, lhs = lhs })
-    vim.api.nvim_buf_set_var(buf, MAPGROUP, maps)
+  local maps = get_buffer_map(buf)
+  table.insert(maps or {}, { modes = modes, lhs = lhs })
+  vim.api.nvim_buf_set_var(buf, MAPGROUP, maps)
 end
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    group = group,
-    callback = function (event)
-        local client_id = event.data.client_id
-        local client = vim.lsp.get_client_by_id(client_id)
-        local bufnr = event.buf
+  group = group,
+  callback = function(event)
+    local client_id = event.data.client_id
+    local client = vim.lsp.get_client_by_id(client_id)
+    local bufnr = event.buf
 
-        -- unnecessary, right? This is just to suppress missing nil check warning
-        if not client then
-            error("No LSP client found")
-        end
-
-        twoslash.setup({
-            highlight = "Comment",
-            is_enabled = true,
-            multi_line = true,
-        })
-        twoslash.enable()
-        twoslash.attach(client, bufnr)
-
-        remember_buffer_map(bufnr,
-            { "n" },
-            "<leader>dq",
-            "<cmd>TwoslashQueriesInspect<CR>", {
-                desc = "Add magic twoslash comment for type introspection",
-            })
-
-        remember_buffer_map(bufnr,
-            { "n" },
-            "<leader>dQ",
-            "<cmd>TwoslashQueriesRemove<CR>", {
-                desc = "Remove all Twoslash queries from buffer",
-            })
+    -- unnecessary, right? This is just to suppress missing nil check warning
+    if not client then
+      error("No LSP client found")
     end
+
+    twoslash.setup({
+      highlight = "Comment",
+      is_enabled = true,
+      multi_line = true,
+    })
+    twoslash.enable()
+    twoslash.attach(client, bufnr)
+
+    remember_buffer_map(bufnr, { "n" }, "<leader>dq", "<cmd>TwoslashQueriesInspect<CR>", {
+      desc = "Add magic twoslash comment for type introspection",
+    })
+
+    remember_buffer_map(bufnr, { "n" }, "<leader>dQ", "<cmd>TwoslashQueriesRemove<CR>", {
+      desc = "Remove all Twoslash queries from buffer",
+    })
+  end,
 })
 
 vim.api.nvim_create_autocmd("LspDetach", {
-    group = group,
-    callback = function(event)
-        local bufnr = event.buf
-        vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+  group = group,
+  callback = function(event)
+    local bufnr = event.buf
+    vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
 
-        twoslash.disable()
-    end,
+    twoslash.disable()
+  end,
 })

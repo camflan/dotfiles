8a645df16a0f10a3ab286bd3abf052928df4e1c8
Camron Flanders <me@camronflanders.com>
2025-05-27 09:56:23 -0500

    add more ai plugins

--- config/nvim/lua/plugins/ai-llm.lua
+++ config/nvim/lua/plugins/ai-llm.lua
@@ -12,7 +12,7 @@ local function get_hostname()
 end
 
 local function get_preferred_model()
-  local preferred_ollama_model = "deepseek-coder-v2:16b"
+  local preferred_ollama_model = "qwen3:30b"
 
   if get_hostname() == "mando" then
     preferred_ollama_model = "starcoder2:3b"
@@ -21,40 +21,112 @@ local function get_preferred_model()
   return preferred_ollama_model
 end
 
+local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+local group = vim.api.nvim_create_augroup("CodeCompanionFidgetHooks", { clear = true })
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = "CodeCompanion*",
+  group = group,
+  callback = function(request)
+    if request.match == "CodeCompanionChatSubmitted" then
+      return
+    end
+
+    local msg
+
+    msg = "[CodeCompanion] " .. request.match:gsub("CodeCompanion", "")
+
+    vim.notify(msg, "info", {
+      timeout = 1000,
+      keep = function()
+        return not vim
+          .iter({ "Finished", "Opened", "Hidden", "Closed", "Cleared", "Created" })
+          :fold(false, function(acc, cond)
+            return acc or vim.endswith(request.match, cond)
+          end)
+      end,
+      id = "code_companion_status",
+      title = "Code Companion Status",
+      opts = function(notif)
+        notif.icon = ""
+        if vim.endswith(request.match, "Started") then
+          ---@diagnostic disable-next-line: undefined-field
+          notif.icon = spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+        elseif vim.endswith(request.match, "Finished") then
+          notif.icon = " "
+        end
+      end,
+    })
+  end,
+})
+
 return {
   {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    lazy = true,
+    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    config = function()
+      require("mcphub").setup()
+    end,
+  },
+
+  {
     "olimorris/codecompanion.nvim",
-    cmd = { "CodeCompanion", "CodeCompanionActions" },
+    cmd = { "CodeCompanion", "CodeCompanionActions", "CodeCompanionChat" },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      "ravitemer/mcphub.nvim",
       -- "stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
     },
-    config = function()
-      require("codecompanion").setup({
-        adapters = {
-          ollama = function()
-            return require("codecompanion.adapters").extend("ollama", {
-              schema = {
-                model = {
-                  default = get_preferred_model(),
-                },
+    opts = {
+      adapters = {
+        ollama = function()
+          return require("codecompanion.adapters").extend("ollama", {
+            schema = {
+              model = {
+                default = get_preferred_model(),
               },
-            })
-          end,
+            },
+          })
+        end,
+      },
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            show_result_in_chat = true,
+            maek_vars = true,
+            make_slash_commands = true,
+          },
         },
-        strategies = {
-          agent = {
-            adapter = "ollama",
-          },
-          chat = {
-            adapter = "ollama",
-          },
-          inline = {
-            adapter = "ollama",
+      },
+      strategies = {
+        agent = {
+          adapter = "ollama",
+        },
+        chat = {
+          adapter = "ollama",
+          slash_commands = {
+            ["file"] = {
+              callback = "strategies.chat.slash_commands.file",
+              description = "Select a file using FZF",
+              opts = {
+                provider = "fzf_lua", -- Can be "default", "telescope", "fzf_lua", "mini_pick" or "snacks"
+                contains_code = true,
+              },
+            },
           },
         },
-      })
+        inline = {
+          adapter = "ollama",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("codecompanion").setup(opts)
     end,
   },
 
@@ -133,5 +205,67 @@ return {
         },
       })
     end,
+  },
+
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = "ollama",
+      ollama = {
+        model = "qwen3:30b",
+        stream = true,
+      },
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+        temperature = 0,
+        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      },
+      selector = {
+        provider = "fzf_lua",
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+          selector = {
+            provider = "fzf_lua",
+          },
+        },
+      },
+    },
   },
 }

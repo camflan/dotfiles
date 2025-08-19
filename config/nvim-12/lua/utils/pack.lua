86daf26bc07d231e7a56889ffd01aea92127e90d
Camron Flanders <me@camronflanders.com>
2025-08-19 15:24:36 -0500

    add autopairs and statusline

--- config/nvim-12/lua/utils/pack.lua
+++ config/nvim-12/lua/utils/pack.lua
@@ -5,10 +5,11 @@ end
 ---@param event table<string>
 ---@param on_load_fn? function
 local make_load_on_event = function(event, on_load_fn)
-    local gr = vim.api.nvim_create_augroup('LoadOn' .. event, {})
+    local group = vim.api.nvim_create_augroup('LoadOn' .. table.concat(event, ":"), {})
+
     return function(spec)
         vim.api.nvim_create_autocmd(event, {
-            group = gr,
+            group = group,
             once = true,
             callback = function() vim.pack.add({ spec.name }) end,
         })
@@ -45,5 +46,6 @@ end
 return {
     add_on_event = add_on_event,
     ensure_installed = ensure_installed,
+    load_on_event = make_load_on_event,
     load_on_filetype = load_on_filetype,
 }
---
+++ config/nvim-12/plugin/01-statusline.lua
@@ -0,0 +1,101 @@
+local utils = require("utils.pack")
+
+utils.ensure_installed({
+    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+})
+
+vim.pack.add({
+    { src = "https://github.com/nvim-lualine/lualine.nvim" }
+}, { load = function (o)
+        vim.cmd.packadd(o.spec.name)
+
+        utils.load_on_event({ "ColorScheme" }, function ()
+            require("lualine").setup({
+                extensions = {
+                    "fugitive",
+                    "fzf",
+                    "lazy",
+                    "mason",
+                    "oil",
+                    "trouble",
+                },
+                options = {
+                    icons_enabled = true,
+                    theme = "auto",
+                    component_separators = { left = "", right = "" },
+                    section_separators = { left = "", right = "" },
+                    disabled_filetypes = {
+                        statusline = {},
+                        winbar = {},
+                    },
+                    ignore_focus = { "oil" },
+                    always_divide_middle = false,
+                    globalstatus = false,
+                    refresh = {
+                        statusline = 100,
+                        tabline = 1000,
+                        winbar = 1000,
+                    },
+                },
+                sections = {
+                    lualine_a = {
+                        {
+                            "mode",
+                            fmt = function(str)
+                                return str:sub(1, 1)
+                            end,
+                        },
+                    },
+                    lualine_b = { "branch", "diff" },
+                    lualine_c = {
+                        {
+                            "filename",
+                            file_status = true,
+                            newfile_status = true,
+                            path = 1,
+                            symbols = {
+                                newfile = "[!]",
+                            },
+                        },
+                    },
+                    lualine_x = {
+                        {
+                            "diagnostics",
+                            -- require("snacks.profiler").status(),
+                            -- sources = { "nvim_diagnostic", "nvim_lsp", "nvim_workspace_diagnostic" },
+                        },
+                    },
+                    lualine_y = {},
+                    lualine_z = {
+                        { "location", separator = "", padding = { left = 1, right = 0 } },
+                        { "progress", separator = "", padding = { left = 1, right = 1 } },
+                    },
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = {
+                        {
+                            "filename",
+                            file_status = true,
+                            newfile_status = true,
+                            path = 2,
+                            shorting_target = 60,
+                            symbols = {
+                                newfile = "[N]",
+                            },
+                        },
+                    },
+                    lualine_x = { "location" },
+                    lualine_y = {},
+                    lualine_z = {},
+                },
+                tabline = {},
+                winbar = {},
+                inactive_winbar = {},
+
+            })
+        end)(o.spec)
+    end
+
+    })

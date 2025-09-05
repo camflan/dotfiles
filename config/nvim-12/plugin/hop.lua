bc668f18661fa2990a4775f5dd370f707d807472
Camron Flanders <me@camronflanders.com>
2025-09-05 08:32:53 -0500

    Add support for HopChar2MW in Neovim 12

--- config/nvim-12/plugin/hop.lua
+++ config/nvim-12/plugin/hop.lua
@@ -9,6 +9,6 @@ vim.pack.add(
 
 require("hop").setup({})
 
-vim.keymap.set({ "n" }, "<leader>W", "<cmd>HopChar2<cr>", { desc = "Hop to bigram" })
+vim.keymap.set({ "n" }, "<leader>W", "<cmd>HopChar2MW<cr>", { desc = "Hop to bigram" })
 vim.keymap.set({ "n" }, "<leader>l", "<cmd>HopLineStart<cr>", { desc = "Hop to a line" })
 vim.keymap.set({ "n" }, "<leader>w", "<cmd>HopWord<cr>", { desc = "Hop to a word" })

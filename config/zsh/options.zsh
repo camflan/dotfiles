cf5f677421ba32aabd50ce7bef3e794f59fb6ff5
Camron Flanders <me@camronflanders.com>
2025-10-23 11:43:25 -0500

    add scripts

---
+++ config/zsh/options.zsh
@@ -0,0 +1,22 @@
+setopt inc_append_history
+setopt share_history
+setopt bang_hist
+setopt hist_verify
+
+setopt autopushd pushdminus pushdsilent pushdtohome
+setopt autocd
+setopt cdable_vars
+
+setopt complete_in_word
+setopt list_ambiguous
+setopt glob_complete
+setopt rec_exact
+
+setopt bad_pattern
+setopt equals
+setopt extended_glob
+setopt glob_dots
+
+setopt noclobber
+setopt correct
+

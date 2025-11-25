741759edde708cd3419a8b93745c3a78c8eba430
Camron Flanders <me@camronflanders.com>
2025-11-25 08:54:16 -0600

    update plugins, add prefix for hop

--- config/nvim/plugin/hop.lua
+++ config/nvim/plugin/hop.lua
@@ -9,6 +9,7 @@ vim.pack.add(
 
 require("hop").setup({})
 
-vim.keymap.set({ "n" }, "<leader>W", "<cmd>HopChar2MW<cr>", { desc = "Hop to bigram" })
-vim.keymap.set({ "n" }, "<leader>l", "<cmd>HopLineStart<cr>", { desc = "Hop to a line" })
 vim.keymap.set({ "n" }, "<leader>w", "<cmd>HopWord<cr>", { desc = "Hop to a word" })
+vim.keymap.set({ "n" }, "<leader>hW", "<cmd>HopChar2MW<cr>", { desc = "Hop to bigram in any window" })
+vim.keymap.set({ "n" }, "<leader>hl", "<cmd>HopLineStart<cr>", { desc = "Hop to a line" })
+vim.keymap.set({ "n" }, "<leader>hw", "<cmd>HopWordMW<cr>", { desc = "Hop to a word in any window" })
